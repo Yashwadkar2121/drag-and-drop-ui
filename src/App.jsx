35a249948049a42c117a54de69d0ea13caa50731
Yash Wadkar <153938514+Yashwadkar2121@users.noreply.github.com>
2024-08-16 03:15:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Canvas from "./components/Canvas";
+
+function App() {
+  return (
+    <div className="w-[100wh] h-[100vh]">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Canvas.jsx
@@ -0,0 +1,172 @@
+import { useState } from "react";
+
+function Canvas() {
+  const [cards, setCards] = useState([
+    {
+      title: "Title",
+      text: "This is some dummy text for the card. It will be shortened.",
+      showFullText: false,
+      editing: false,
+    },
+  ]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editedCard, setEditedCard] = useState({});
+  const handleAddCard = () => {
+    setCards([
+      ...cards,
+      {
+        title: `Title ${cards.length + 1}`,
+        text: "This is some dummy text for the card. It will be shortened.",
+        showFullText: false,
+      },
+    ]);
+  };
+
+  const handleShowMore = (index) => {
+    setCards(
+      cards.map((card, i) => {
+        if (i === index) {
+          return { ...card, showFullText: true };
+        }
+        return card;
+      })
+    );
+  };
+
+  const handleShowLess = (index) => {
+    setCards(
+      cards.map((card, i) => {
+        if (i === index) {
+          return { ...card, showFullText: false };
+        }
+        return card;
+      })
+    );
+  };
+
+  const handleEdit = (index) => {
+    setModalOpen(true);
+    setEditedCard({ ...cards[index], index });
+  };
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  };
+
+  const handleSave = () => {
+    const updatedCards = cards.map((card, i) => {
+      if (i === editedCard.index) {
+        return { ...card, title: editedCard.title, text: editedCard.text };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+    setModalOpen(false);
+  };
+
+  const handleDelete = (index) => {
+    setCards(cards.filter((card, i) => i !== index));
+  };
+  
+  return (
+    <div className="m-10">
+      <div className="flex justify-center ">
+        {" "}
+        <button
+          onClick={handleAddCard}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full"
+        >
+          Add Card
+        </button>
+      </div>
+
+      <div className="mt-10 flex flex-wrap justify-center gap-3 items-center ">
+        {cards.map((card, index) => [
+          <div
+            key={index}
+            className="bg-blue-400 p-5 rounded-2xl mt-10 w-96 border border-gray-700 relative"
+          >
+            <div className="flex justify-between">
+              <h3 className="font-bold text-2xl">{card.title}</h3>
+              <div className="flex gap-5">
+                <button onClick={() => handleDelete(index)}>
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+                <button onClick={() => handleEdit(index)}>
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+              </div>
+            </div>
+            <p className="font-medium">
+              {card.showFullText
+                ? card.text
+                : card.text.substring(0, 20) + " ..."}
+              {card.showFullText ? (
+                <button
+                  onClick={() => handleShowLess(index)}
+                  className=" text-black font-bold py-1 px-2 rounded-full"
+                >
+                  Show Less
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleShowMore(index)}
+                  className=" text-black font-bold py-1 px-2 rounded-full"
+                >
+                  Show More
+                </button>
+              )}
+            </p>
+          </div>,
+
+          // index < cards.length - 1 && (
+          //   <div className="text-black text-2xl " key={`yash-${index}`}>
+          //     yash
+          //   </div>
+          // ),
+        ])}
+      </div>
+
+      {modalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 flex justify-center">
+          <div className="bg-white p-5 rounded-2xl mt-10 w-96 border border-gray-700 h-72 ">
+            <h3 className="font-bold text-2xl text-black text-center">
+              Edit Card
+            </h3>
+            <input
+              type="text"
+              value={editedCard.title}
+              onChange={(e) =>
+                setEditedCard({ ...editedCard, title: e.target.value })
+              }
+              className="mt-5 w-full p-2 pl-3 text-sm text-gray-700 border border-black rounded-full"
+            />
+            <textarea
+              value={editedCard.text}
+              onChange={(e) =>
+                setEditedCard({ ...editedCard, text: e.target.value })
+              }
+              className="mt-5 w-full p-2 pl-3 text-sm text-gray-700 border border-black rounded-xl"
+            />
+            <div className="flex justify-end mt-5">
+              <button
+                onClick={handleSave}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 rounded-full mr-2"
+              >
+                Save
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-semibold py-1 px-2 rounded-full"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Canvas;
